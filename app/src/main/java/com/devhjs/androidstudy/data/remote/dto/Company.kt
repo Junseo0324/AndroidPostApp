02995af43f0cab4288fbf120bff7ff92636e1e13
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-05-01 09:57:13 +0900

    feat: 사용자 정보 조회를 위한 DTO 클래스 추가
    * `UserDto`, `Address`, `Company`, `Geo` 등 사용자 관련 데이터 전송 객체 정의
    * Kotlinx Serialization(@Serializable)을 적용하여 JSON 역직렬화 환경 구축

---
+++ app/src/main/java/com/devhjs/androidstudy/data/remote/dto/Company.kt
@@ -0,0 +1,10 @@
+package com.devhjs.androidstudy.data.remote.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Company(
+    val bs: String,
+    val catchPhrase: String,
+    val name: String
+)
---
+++ app/src/main/java/com/devhjs/androidstudy/data/remote/dto/UserDto.kt
@@ -0,0 +1,15 @@
+package com.devhjs.androidstudy.data.remote.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserDto(
+    val address: Address,
+    val company: Company,
+    val email: String,
+    val id: Int,
+    val name: String,
+    val phone: String,
+    val username: String,
+    val website: String
+)
