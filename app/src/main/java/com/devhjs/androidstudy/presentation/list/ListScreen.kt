87c10de53c004f0140c4035a2a2bbeda20fedfd4
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-05-07 19:46:32 +0900

    refactor: ListScreen의 레이아웃 구조 및 클릭 액션 수정
    * Row 내 요소 간격 설정을 `Spacer` 대신 `Arrangement.spacedBy(16.dp)`를 사용하도록 변경
    * 하단 Todo 항목의 너비를 `fillMaxWidth(0.5f)`로 지정하여 레이아웃 수정
    * 하단 Column의 클릭 액션을 `OnAlbumClick`에서 `OnTodoClick`으로 수정하여 올바른 액션이 수행되도록 변경

--- app/src/main/java/com/devhjs/androidstudy/presentation/list/ListScreen.kt
+++ app/src/main/java/com/devhjs/androidstudy/presentation/list/ListScreen.kt
@@ -7,9 +7,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,7 +31,8 @@ fun ListScreen(
             .padding(16.dp)
     ) {
         Row(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             Column(
                 modifier = Modifier
@@ -48,7 +49,6 @@ fun ListScreen(
                 Spacer(modifier = Modifier.height(8.dp))
                 Text(state.postSize.toString(),fontSize = 20.sp)
             }
-            Spacer(modifier = Modifier.width(16.dp))
             Column(
                 modifier = Modifier
                     .weight(1f)
@@ -65,13 +65,14 @@ fun ListScreen(
                 Text(state.albumSize.toString(), fontSize = 20.sp)
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
         Column(
             modifier = Modifier
-                .weight(1f)
+                .fillMaxWidth(0.5f)
                 .height(150.dp)
                 .border(1.dp, Color.Black, shape = RoundedCornerShape(8.dp))
                 .clickable {
-                    onAction(ListAction.OnAlbumClick)
+                    onAction(ListAction.OnTodoClick)
                 },
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
