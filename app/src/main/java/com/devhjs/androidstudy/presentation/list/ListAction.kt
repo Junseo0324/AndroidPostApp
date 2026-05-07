29d6d3032ae23e4345cc4facd47562f84c553bce
Heo Junseo <107256859+Junseo0324@users.noreply.github.com>
2026-05-07 19:41:46 +0900

    feat: Todo 화면 이동을 위한 ListAction 및 ListEvent 추가
    * 사용자가 Todo 항목을 클릭했을 때의 동작을 정의하기 위해 ListAction에 OnTodoClick 추가
    * Todo 화면으로의 내비게이션 처리를 위해 ListEvent에 OnNavigateToTodo 추가

--- app/src/main/java/com/devhjs/androidstudy/presentation/list/ListAction.kt
+++ app/src/main/java/com/devhjs/androidstudy/presentation/list/ListAction.kt
@@ -3,4 +3,5 @@ package com.devhjs.androidstudy.presentation.list
 sealed interface ListAction {
     data object OnPostClick : ListAction
     data object OnAlbumClick : ListAction
+    data object OnTodoClick : ListAction
 }
--- app/src/main/java/com/devhjs/androidstudy/presentation/list/ListEvent.kt
+++ app/src/main/java/com/devhjs/androidstudy/presentation/list/ListEvent.kt
@@ -3,4 +3,5 @@ package com.devhjs.androidstudy.presentation.list
 sealed interface ListEvent {
     data class OnNavigateToPost(val userId: Int) : ListEvent
     data class OnNavigateToAlbum(val userId: Int) : ListEvent
+    data class OnNavigateToTodo(val userId: Int) : ListEvent
 }
